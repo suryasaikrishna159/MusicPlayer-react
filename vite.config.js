20169cdc594362bc3bc273c91bba4ef3dff36dcc
suryasaikrishna159 <suryasada222@gmail.com>
2025-06-23 14:17:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/fontawesome-free'],
-    },
-  },
   plugins: [react()],
-});
+})
