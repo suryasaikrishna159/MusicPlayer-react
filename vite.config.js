75c027619c7516e3d0692aee498011d35c48366c
suryasaikrishna159 <suryasada222@gmail.com>
2025-06-23 14:07:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/fontawesome-free']
+    }
+  }
+});
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
